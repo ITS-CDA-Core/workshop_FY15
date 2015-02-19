fdeebcf5cf887d8403f4db3ab7a49103ab62e11c
imaizm@gmail.com <imaizm@gmail.com>
2015-02-20 00:36:07 +0900

    Spring-Bootアプリ定義にViewとしてJSP定義を追加

--- chef/site-cookbooks/java/recipes/spring-boot_with_maven.rb
+++ chef/site-cookbooks/java/recipes/spring-boot_with_maven.rb
@@ -1,5 +1,5 @@
 
-directory node['spring-boot']['app_dir'] do
+directory "#{node['spring-boot']['app_dir']}/src/main/resources" do
 	recursive true
 	action :create
 end
@@ -15,6 +15,14 @@ template "pom.xml" do
 		:artifactId => node['spring-boot']['artifactId'],
 		:versionOfSpringBoot => node['spring-boot']['version']
 	})
+end
+
+template "application.properties" do
+	path "#{node['spring-boot']['app_dir']}/src/main/resources/application.properties"
+	owner "vagrant"
+	group "vagrant"
+	mode "0644"
+	source "application.properties_for_spring-boot.erb"
 end
 
 bash "package spring-boot project with maven" do
--- src/1st-app/src/main/java/org/sample/RootController.java
+++ src/1st-app/src/main/java/org/sample/RootController.java
@@ -1,15 +1,16 @@
 package org.sample;
 
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;;
 
 @Controller
 public class RootController
 {
 
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    String index(Model model) {
+    	model.addAttribute("message", "this is index page!");
+        return "Root";
     }
 }
