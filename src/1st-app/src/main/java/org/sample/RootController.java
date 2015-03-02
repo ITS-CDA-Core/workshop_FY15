6cd49a8befc1e37bc03b88ed2d8d9586d0ece4df
imaizm <imaizm@gmail.com>
2015-03-03 00:30:30 +0900

    space->tab

--- src/1st-app/src/main/java/org/sample/RootController.java
+++ src/1st-app/src/main/java/org/sample/RootController.java
@@ -11,17 +11,17 @@ import org.springframework.web.servlet.ModelAndView;
 public class RootController
 {
 
-    @RequestMapping("/")
-    String index(Model model) {
-    	model.addAttribute("message", "this is index page!");
-        return "Root";
-    }
-    
-    @RequestMapping(value="/post", method=RequestMethod.POST)
-    public ModelAndView postForm(
-    	@RequestParam("text1") String text1) {
-    	ModelAndView mv = new ModelAndView("Root");
-    	mv.addObject("message", "you typed '" + text1 + "'");
-    	return mv;
-    }
+	@RequestMapping("/")
+	String index(Model model) {
+		model.addAttribute("message", "this is index page!");
+		return "Root";
+	}
+
+	@RequestMapping(value="/post", method=RequestMethod.POST)
+	public ModelAndView postForm(
+		@RequestParam("text1") String text1) {
+		ModelAndView mv = new ModelAndView("Root");
+		mv.addObject("message", "you typed '" + text1 + "'");
+		return mv;
+	}
 }
